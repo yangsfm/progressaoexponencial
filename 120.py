7b3e64b78c7619c10ee6565521ded29abaa72964
Yang Sarmento Ferreira <60950025+yangsfm@users.noreply.github.com>
2020-03-03 22:02:27 -0300

    Add files via upload

---
+++ 120.py
@@ -0,0 +1,12 @@
+#Autores: 1DS3
+#	@Gabriel Viana Ribeiro
+#	@Yang Sarmento Ferreira
+#Feito em:03/03/2020
+#Instituição Etec de Itanhaém
+
+x = 0 #Declaração do contador
+valor = 2 #Declaração da variável valor
+while (x <= 21): 
+		valor = valor * valor
+		x = x + 1 #Laço de repetição 
+print(valor) #Exibir a variável valor
